e1d09a1e896a2560734058ebc7bc893108dea816
gorika1 <valentulio@compear.net>
2014-05-26 08:10:14 -0400

    Primera version

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	'Master Page' => array(
+		'HEAD' 		=> 'head',
+		'HEADER' 	=> 'header',
+		'FOOTER' 	=> 'footer',
+	),
+
+	'Data Base' => array(
+		'Host' 		=> 'sql102.byethost13.com',
+		'User' 		=> 'b13_13268400',
+		'Password'	=>	'talitax',
+		'DB'		=> 'b13_13268400_honolulu',
+	),
+);
